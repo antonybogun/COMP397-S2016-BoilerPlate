bff435b7b5d03e2f26bae2d9ec604d0f12c39ef3
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-07-11 19:12:27 -0400

    updated master branch before merge

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -1,10 +1,25 @@
 /// <reference path="_reference.ts"/>
+/**
+ * @author Tom Tsiliopoulos ttsliop@my.centennialcollege.ca
+ * @studentID 300818577
+ * @date July 4, 2016
+ * @description This file is the entry point for the game
+ * @version 0.01 - Initial version of the boilerplate
+ */
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Variable Declarations
 // make a reference to the canvas element
 var canvas = document.getElementById("canvas");
 // create a reference to a stage container
 var stage;
 var helloLabel;
 var startButton; // reference to our button class
+/**
+ * This method is the entry point for the application
+ *
+ * @method init
+ * @return {void}
+ */
 function init() {
     stage = new createjs.Stage(canvas); // instatiate the stage container
     stage.enableMouseOver(20);
@@ -12,9 +27,21 @@ function init() {
     createjs.Ticker.on("tick", gameLoop); // create an event listener for the tick event
     main(); // call the main game function
 }
-function gameLoop() {
+/**
+ * This is the main game loop
+ *
+ * @method gameLoop
+ * @param {createjs.Event} event
+ * @returns {void}
+ */
+function gameLoop(event) {
     stage.update(); // refreshes the stage
 }
+/**
+ * This is the startButton click event handler
+ *
+ * @param {createjs.MouseEvent} event
+ */
 function startButtonClick(event) {
     helloLabel.text = "clicked!";
 }
@@ -35,5 +62,7 @@ function main() {
     stage.addChild(startButton);
     startButton.on("click", startButtonClick);
 }
+//wait until the window object is finished loading then call the init method
 window.addEventListener("load", init);
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ 
 //# sourceMappingURL=game.js.map
